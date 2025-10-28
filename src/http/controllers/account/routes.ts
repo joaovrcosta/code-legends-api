2180ee25cb2d79ad5b7dd29a6d9e28feb7ee4e28
joaovrcosta <joaovr.costa@gmail.com>
2025-10-28 18:01:07 -0300

    feat: added refresh token

--- src/http/controllers/account/routes.ts
+++ src/http/controllers/account/routes.ts
@@ -2,11 +2,13 @@ import { FastifyInstance } from "fastify";
 import { create } from "./create.controller";
 import { authenticate } from "./authenticate.controller";
 import { profile } from "./profile.controller";
+import { refreshToken } from "./refresh-token.controller";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", create);
   app.post("/users/auth", authenticate);
+  app.post("/token/refresh", refreshToken);
 
   // Rotas autenticadas
   app.get("/me", { onRequest: [verifyJWT] }, profile);
