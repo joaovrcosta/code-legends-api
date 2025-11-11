a15c1b088bff0193343c79c35baa84ff0ce789fb
joaovrcosta <joaovr.costa@gmail.com>
2025-11-11 14:43:35 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from "./env";
 app
   .listen({
     host: "0.0.0.0",
-    port: env.PORT,
+    port: env.PORT ? Number(env.PORT) : 3333,
   })
   .then(() => {
     console.log("Server is Running...");
